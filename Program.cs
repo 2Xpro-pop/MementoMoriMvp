28b6cf377a654dc87b58e930883733c4fd373819
2Xpro-pop <bay.asanov@mail.ru>
2022-10-19 08:42:32 +0600

    try fix

--- Program.cs
+++ Program.cs
@@ -11,6 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton<AccountImitatorService>();
 builder.Services.AddMudServices();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress+ "/MementoMoriMvp") });
 
 await builder.Build().RunAsync();
